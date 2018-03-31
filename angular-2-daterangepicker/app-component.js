111171b4a5675e832ae4a4ac1519fb842f5831eb
nikhil-001mehta <nikhil.001mhehta@gmail.com>
2018-03-31 19:21:53 +0530

    Adding config generator to anfgular-2-daterangepicker

--- angular-2-daterangepicker/app-component.js
+++ angular-2-daterangepicker/app-component.js
@@ -12,15 +12,66 @@ var core_1 = require('@angular/core');
 var moment = require('moment');
 var AppComponent = (function () {
     function AppComponent() {
-        this.daterangepickerOptions = {};
+        this.daterangepickerOptions = {
+            startDate: null,
+            endDate: null,
+            format: "DD.MM.YYYY",
+            minDate: moment().add(-2, 'months').format("DD.MM.YYYY"),
+            maxDate: moment().add(2, 'months').format("DD.MM.YYYY"),
+            inactiveBeforeStart: true,
+            autoApply: false,
+            showRanges: true,
+            preDefinedRanges: [{
+                    name: 'Day After tomorrow',
+                    value: {
+                        start: moment().add(2, 'days'),
+                        end: moment().add(2, 'days'),
+                    }
+                }, {
+                    name: 'Today',
+                    value: {
+                        start: moment(),
+                        end: moment(),
+                    }
+                }, {
+                    name: 'Tomorrow',
+                    value: {
+                        start: moment().add(1, 'days'),
+                        end: moment().add(1, 'days'),
+                    }
+                }, {
+                    name: 'This week',
+                    value: {
+                        start: moment(),
+                        end: moment().add(7, 'days'),
+                    }
+                }],
+            singleCalendar: false,
+            displayFormat: 'DD.MMM'
+        };
     }
     AppComponent.prototype.rangeSelected = function (data) {
         debugger;
     };
+    AppComponent.prototype.singleCalendar = function (event) {
+        this.daterangepickerOptions.singleCalendar = event.target.checked;
+    };
+    AppComponent.prototype.autoApply = function (event) {
+        this.daterangepickerOptions.autoApply = event.target.checked;
+    };
+    AppComponent.prototype.inactiveBeforeStart = function (event) {
+        this.daterangepickerOptions.inactiveBeforeStart = event.target.checked;
+    };
+    AppComponent.prototype.showRanges = function (event) {
+        this.daterangepickerOptions.showRanges = event.target.checked;
+    };
+    AppComponent.prototype.prettyPrintJSON = function (object) {
+        return JSON.stringify(object, null, '  ');
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: "my-datepicker-demo",
-            template: "\n\t\t<div class=\"col-md-2\">\n\t\t\t<date-range-picker [class]=\"'col-md-12 form-control'\" [options]=\"daterangepickerOptions\" (rangeSelected)=\"rangeSelected($event)\">\n\t\t\t</date-range-picker>\n\t\t</div>\n\t"
+            template: "\n\t\t<div class=\"col-md-7 separator-right\">\n\t\t\t<h3>Configuration Options Tester</h3>\n\t\t\t<div class=\"col-md-12\">\n\t\t\t\t<input type=\"checkbox\" checked={{daterangepickerOptions.singleCalendar}} id=\"singleCalendar\" (click)=\"singleCalendar($event)\">\n\t\t\t\t<label class=\"label\" for=\"singleCalendar\">singleCalendar</label>\n\t\t\t</div>\n\t\t\t<div class=\"col-md-12\">\n\t\t\t\t<input type=\"checkbox\" checked={{daterangepickerOptions.autoApply}} id=\"autoApply\" (click)=\"autoApply($event)\">\n\t\t\t\t<label class=\"label\" for=\"autoApply\">autoApply</label>\n\t\t\t</div>\n\t\t\t<div class=\"col-md-12\">\n\t\t\t\t<input type=\"checkbox\" checked={{daterangepickerOptions.inactiveBeforeStart}} id=\"inactiveBeforeStart\" (click)=\"inactiveBeforeStart($event)\">\n\t\t\t\t<label class=\"label\" for=\"inactiveBeforeStart\">inactiveBeforeStart</label>\n\t\t\t</div>\n\t\t\t<div class=\"col-md-12\">\n\t\t\t\t<input type=\"checkbox\" checked={{daterangepickerOptions.showRanges}} id=\"showRanges\" (click)=\"showRanges($event)\">\n\t\t\t\t<label class=\"label\" for=\"showRanges\">showRanges</label>\n\t\t\t</div>\n\t\t\t<div class=\"col-md-12\">\n\t\t\t\t<h4>Configuration</h4>\n\t\t\t\t<pre>{{prettyPrintJSON(daterangepickerOptions)}}</pre>\n\t\t\t</div>\n\t\t</div> \n\t\t<div class=\"col-md-5\">\n\t\t\t<h3>Live Demo</h3>\t\n\t\t\t<div class=\"col-md-6 flush\">\n\t\t\t\t<date-range-picker [class]=\"'col-md-12 form-control'\" [options]=\"daterangepickerOptions\" (rangeSelected)=\"rangeSelected($event)\">\n\t\t\t\t</date-range-picker>\n\t\t\t</div>\n\t\t</div>\n\t"
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
