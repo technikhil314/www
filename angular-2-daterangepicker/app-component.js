7e4dd15dde7850205443102df4b59cb52f48913a
nikhil-001mehta <nikhil.001mhehta@gmail.com>
2018-06-16 19:09:40 +0530

    syncing with latest changes in module

--- angular-2-daterangepicker/app-component.js
+++ angular-2-daterangepicker/app-component.js
@@ -1,8 +1,12 @@
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    var c = arguments.length,
+        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
+        d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    else
+        for (var i = decorators.length - 1; i >= 0; i--)
+            if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var __metadata = (this && this.__metadata) || function (k, v) {
@@ -23,30 +27,30 @@ var AppComponent = (function () {
             autoApply: true,
             showRanges: true,
             preDefinedRanges: [{
-                    name: 'Day After tomorrow',
-                    value: {
-                        start: moment().add(2, 'days'),
-                        end: moment().add(2, 'days'),
-                    }
-                }, {
-                    name: 'Today',
-                    value: {
-                        start: moment(),
-                        end: moment(),
-                    }
-                }, {
-                    name: 'Tomorrow',
-                    value: {
-                        start: moment().add(1, 'days'),
-                        end: moment().add(1, 'days'),
-                    }
-                }, {
-                    name: 'This week',
-                    value: {
-                        start: moment(),
-                        end: moment().add(7, 'days'),
-                    }
-                }],
+                name: 'Day After tomorrow',
+                value: {
+                    start: moment().add(2, 'days'),
+                    end: moment().add(2, 'days'),
+                }
+            }, {
+                name: 'Today',
+                value: {
+                    start: moment(),
+                    end: moment(),
+                }
+            }, {
+                name: 'Tomorrow',
+                value: {
+                    start: moment().add(1, 'days'),
+                    end: moment().add(1, 'days'),
+                }
+            }, {
+                name: 'This week',
+                value: {
+                    start: moment(),
+                    end: moment().add(7, 'days'),
+                }
+            }],
             singleCalendar: false,
             displayFormat: 'DD.MM.YYYY HH:mm',
             position: 'left',
@@ -76,11 +80,11 @@ var AppComponent = (function () {
     AppComponent.prototype.setTimePicker = function (event) {
         this.isTimePickerEnabled = event.target.checked;
         this.daterangepickerOptions.timePicker = event.target.checked ? {
-            minuteInterval: 5
+            minuteInterval: 5,
+            twentyFourHourFormat: this.daterangepickerOptions.timePicker && !!this.daterangepickerOptions.timePicker.twentyFourHourFormat
         } : null;
     };
-    AppComponent.prototype.setPosition = function () {
-    };
+    AppComponent.prototype.setPosition = function () {};
     AppComponent.prototype.prettyPrintJSON = function (object) {
         return JSON.stringify(object, null, '  ');
     };
@@ -88,7 +92,7 @@ var AppComponent = (function () {
         core_1.Component({
             selector: "my-datepicker-demo",
             template: "\n\t\t<div class=\"col-md-7 separator-right\">\n\t\t\t<h3>Configuration Options Tester</h3>\n\t\t\t<form>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"singleCalendar\" [(ngModel)]=daterangepickerOptions.singleCalendar id=\"singleCalendar\">\n\t\t\t\t\t<label class=\"label\" for=\"singleCalendar\">Single Calendar</label>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"autoApply\" [(ngModel)]=daterangepickerOptions.autoApply id=\"autoApply\">\n\t\t\t\t\t<label class=\"label\" for=\"autoApply\">Auto Apply</label>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"inactiveBeforeStart\" [(ngModel)]=daterangepickerOptions.inactiveBeforeStart id=\"inactiveBeforeStart\">\n\t\t\t\t\t<label class=\"label\" for=\"inactiveBeforeStart\">Inactive Before Start</label>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"disableBeforeStart\" [(ngModel)]=daterangepickerOptions.disableBeforeStart id=\"disableBeforeStart\">\n\t\t\t\t\t<label class=\"label\" for=\"disableBeforeStart\">Disable Before Start</label>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"disabled\" [(ngModel)]=daterangepickerOptions.disabled id=\"disabled\">\n\t\t\t\t\t<label class=\"label\" for=\"disabled\">Disabled</label>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"showRanges\" [(ngModel)]=\"daterangepickerOptions.showRanges\" id=\"showRanges\">\n\t\t\t\t\t<label class=\"label\" for=\"showRanges\">Show Ranges</label>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"noDefaultRangeSelected\" [(ngModel)]=\"daterangepickerOptions.noDefaultRangeSelected\" id=\"noDefaultRangeSelected\">\n\t\t\t\t\t<label class=\"label\" for=\"noDefaultRangeSelected\">No Default Range Selected</label>\n\t\t\t\t</div>\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<input type=\"checkbox\" name=\"timepicker\" [ngModel]=\"isTimePickerEnabled\" (change)=\"setTimePicker($event)\" id=\"timepicker\">\n\t\t\t\t\t<label class=\"label\" for=\"timepicker\">TimePicker</label>\n\t\t\t\t</div>\t\n\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t<label class=\"label\">Position</label>\n\t\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t\t<label class=\"label\" for=\"position-left\">Left</label>\n\t\t\t\t\t\t<input type=\"radio\" name=\"position\" value=\"left\" [(ngModel)]=\"daterangepickerOptions.position\" id=\"position-left\">\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t\t<label class=\"label\" for=\"position-right\">Right</label>\n\t\t\t\t\t\t<input type=\"radio\" name=\"position\" value=\"right\" [(ngModel)]=\"daterangepickerOptions.position\" id=\"position-right\">\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"col-md-12\">\n\t\t\t\t\t\t<label class=\"label\" for=\"position-left\">Center</label>\n\t\t\t\t\t\t<input type=\"radio\" name=\"position\" value=\"center\" [(ngModel)]=\"daterangepickerOptions.position\" id=\"position-center\">\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</form>\n\t\t\t<div class=\"col-md-12\">\n\t\t\t\t<h4>Configuration</h4>\n\t\t\t\t<pre>{{prettyPrintJSON(daterangepickerOptions)}}</pre>\n\t\t\t</div>\n\t\t</div> \n\t\t<div class=\"col-md-5\">\n\t\t\t<h3>Live Demo</h3>\t\n\t\t\t<div class=\"col-md-6 flush\">\n\t\t\t\t<date-range-picker [class]=\"'col-md-12 form-control'\" [options]=\"daterangepickerOptions\" (rangeSelected)=\"rangeSelected($event)\">\n\t\t\t\t</date-range-picker>\n\t\t\t</div>\n\t\t</div>\n\t"
-        }), 
+        }),
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
