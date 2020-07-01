93c338569c5378c5718bed78b68c1e5195028ee7
Nikhil Mehta <nikhil.001mehta@gmail.com>
2020-07-01 18:00:13 +0530

    pagebased multiple sidebars achieved

---
+++ main/sidebars.js
@@ -0,0 +1,12 @@
+module.exports = {
+  firstSidebar: {
+    'Category A': ['doc1'],
+  },
+  secondSidebar: {
+    'Category A': ['doc2'],
+    'Category B': ['doc3'],
+  },
+  thirdSidebar: {
+    'Category Z': ['doc11'],
+  },
+};
