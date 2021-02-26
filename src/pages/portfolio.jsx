1d17b01b0cec3f3ac06b31d2fa6b8f7f075ef613
Nikhil Mehta <nikhil.001mehta@gmail.com>
2021-02-26 13:31:54 +0530

    fix: keeping layout on portfolio page

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,13 +1,12 @@
-
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import React from "react";
 export default function Portfolio() {
-  // const context = useDocusaurusContext();
-  // const { siteConfig = {} } = context;
-  // return (
-  //   <Layout title={`Portfolio`} description={siteConfig.tagline}>
-  //     <main>
-  //       <Box position={[0, 0, 0]} />
-  //     </main>
-  //   </Layout>
-  // );
-  return null;
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+  return (
+    <Layout title={`Portfolio`} description={siteConfig.tagline}>
+      <main>{/* <Box position={[0, 0, 0]} /> */}</main>
+    </Layout>
+  );
 }
