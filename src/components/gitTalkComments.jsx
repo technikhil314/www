133678f8a20b2c1aebfe39d92756e0342ab4ef17
Nikhil Mehta <nikhil.001mehta@gmail.com>
2021-04-16 14:27:46 +0530

    fix: git talk styles break on refresh of page

--- src/components/gitTalkComments.jsx
+++ src/components/gitTalkComments.jsx
@@ -7,17 +7,19 @@ export default function GitTalkComments() {
     <BrowserOnly fallback={<div>Loading comments...</div>}>
       {() => {
         return (
-          <GitalkComponent
-            options={{
-              clientID: "6a7ee7fe7d7cae5cd831",
-              clientSecret: "eeab5ad001eef8695cb95b3c5a487a05508045e9",
-              repo: "www",
-              owner: "technikhil314",
-              admin: ["technikhil314"],
-              id: location.pathname.split("blog/")[1].substring(0, 40),
-              distractionFreeMode: false,
-            }}
-          />
+          <div className="gt-container">
+            <GitalkComponent
+              options={{
+                clientID: "6a7ee7fe7d7cae5cd831",
+                clientSecret: "eeab5ad001eef8695cb95b3c5a487a05508045e9",
+                repo: "www",
+                owner: "technikhil314",
+                admin: ["technikhil314"],
+                id: location.pathname.split("blog/")[1].substring(0, 40),
+                distractionFreeMode: false,
+              }}
+            />
+          </div>
         );
       }}
     </BrowserOnly>
