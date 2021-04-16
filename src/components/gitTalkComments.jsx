d051152f6688296ed6ee44dacbcc30c4bf6be66a
Nikhil Mehta <nikhil.001mehta@gmail.com>
2021-04-16 14:00:13 +0530

    fix: fixed SSG of blogs
    Gittalk needs to be CSR

--- src/components/gitTalkComments.jsx
+++ src/components/gitTalkComments.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 import GitalkComponent from "gitalk/dist/gitalk-component";
+import React from "react";
+
 export default function GitTalkComments() {
   return (
-    <>
-      <GitalkComponent
-        options={{
-          clientID: "6a7ee7fe7d7cae5cd831",
-          clientSecret: "eeab5ad001eef8695cb95b3c5a487a05508045e9",
-          repo: "www",
-          owner: "technikhil314",
-          admin: ["technikhil314"],
-          id: location.pathname.substring(0, 40),
-          distractionFreeMode: false,
-        }}
-      />
-    </>
+    <BrowserOnly fallback={<div>Loading comments...</div>}>
+      {() => {
+        return (
+          <GitalkComponent
+            options={{
+              clientID: "6a7ee7fe7d7cae5cd831",
+              clientSecret: "eeab5ad001eef8695cb95b3c5a487a05508045e9",
+              repo: "www",
+              owner: "technikhil314",
+              admin: ["technikhil314"],
+              id: location.pathname.substring(0, 40),
+              distractionFreeMode: false,
+            }}
+          />
+        );
+      }}
+    </BrowserOnly>
   );
 }
