c2a664af0db2f87c6f8f68cae4cbdf38a9bb764a
Nikhil Mehta <nikhil.001mehta@gmail.com>
2021-04-17 19:50:14 +0530

    fix: added back to blog list links
    Removed TOC and blog left sidebar from blog post page
    added max width to blog content

---
+++ src/components/backTo.jsx
@@ -0,0 +1,26 @@
+import Link from "@docusaurus/Link";
+import React from "react";
+
+export default function BackTo({ url, text }) {
+  return (
+    <Link
+      to={url}
+      className="col col--12 padding-left--none margin-bottom--lg inline-flex align-items-center"
+    >
+      <svg
+        className="margin-right--sm"
+        viewBox="0 0 15 15"
+        fill="none"
+        style={{
+          height: "100%",
+          width: "1em",
+          position: "relative",
+          top: "2px",
+        }}
+      >
+        <path d="M1.5 7.5l4-4m-4 4l4 4m-4-4H14" stroke="currentColor"></path>
+      </svg>
+      {text}
+    </Link>
+  );
+}
--- src/components/blogPost.jsx
+++ src/components/blogPost.jsx
@@ -1,10 +1,10 @@
 import BlogPostItem from "@theme/BlogPostItem";
 import BlogPostPaginator from "@theme/BlogPostPaginator";
-import BlogSidebar from "@theme/BlogSidebar";
 import IconEdit from "@theme/IconEdit";
 import Layout from "@theme/Layout";
 import TOC from "@theme/TOC";
 import React from "react";
+import BackTo from "./backTo";
 import Comments from "./comments";
 
 function BlogPostPage(props) {
@@ -17,15 +17,13 @@ function BlogPostPage(props) {
     <Layout
       title={title}
       description={description}
-      wrapperClassName="blog-wrapper"
+      wrapperClassName="blog-wrapper "
     >
       {BlogPostContents && (
         <div className="container margin-vert--lg">
-          <div className="row">
-            <div className="col col--2">
-              <BlogSidebar sidebar={sidebar} />
-            </div>
-            <main className="col col--8 position--relative">
+          <div className="row justify-center">
+            <main className="col position--relative max-w-blog">
+              <BackTo url="/blog" text="Back to blog list"></BackTo>
               <BlogPostItem
                 frontMatter={frontMatter}
                 metadata={metadata}
@@ -33,6 +31,7 @@ function BlogPostPage(props) {
               >
                 <BlogPostContents />
               </BlogPostItem>
+              <BackTo url="/blog" text="Back to blog list"></BackTo>
               <div>
                 {editUrl && (
                   <a href={editUrl} target="_blank" rel="noreferrer noopener">
@@ -42,7 +41,7 @@ function BlogPostPage(props) {
                 )}
               </div>
               {(nextItem || prevItem) && (
-                <div className="margin-vert--xl">
+                <div className="margin-vert--lg">
                   <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />
                 </div>
               )}
