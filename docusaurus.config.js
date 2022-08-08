4da5e436e0c154e1c7ca1cc78b5d8cca70ff1381
Nikhil Mehta <nikhil.001mehta@gmail.com>
2022-08-08 16:04:28 +0530

    docs: aaded poems

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,27 +1,34 @@
-const myopenSourceWorkItems = [{
-  to: "autobadger",
-  label: "Auto Badger",
-}, {
-  to: "daterangepicker",
-  label: "Angular Date Range Picker",
-}, {
-  to: "carousel",
-  label: "Carousel Component",
-}, {
-  href: "https://openrtc.vercel.app/",
-  label: "OpenRTC",
-}, {
-  href: "https://verifyemail.vercel.app/",
-  label: "Verify email - Open API"
-}, {
-  href: "https://diffviewer.vercel.app/",
-  label: "Diff viewer"
-}];
+const myopenSourceWorkItems = [
+  {
+    to: "autobadger",
+    label: "Auto Badger",
+  },
+  {
+    to: "daterangepicker",
+    label: "Angular Date Range Picker",
+  },
+  {
+    to: "carousel",
+    label: "Carousel Component",
+  },
+  {
+    href: "https://openrtc.vercel.app/",
+    label: "OpenRTC",
+  },
+  {
+    href: "https://verifyemail.vercel.app/",
+    label: "Verify email - Open API",
+  },
+  {
+    href: "https://diffviewer.vercel.app/",
+    label: "Diff viewer",
+  },
+];
 
 const sponsorMeItems = [
   {
-    href: 'https://www.buymeacoffee.com/technikhil314',
-    label: 'Buy me a coffee',
+    href: "https://www.buymeacoffee.com/technikhil314",
+    label: "Buy me a coffee",
   },
   {
     href: "https://paypal.me/technikhil314",
@@ -29,134 +36,157 @@ const sponsorMeItems = [
   },
   {
     href: "https://github.com/sponsors/technikhil314",
-    label: "Github"
-  }
+    label: "Github",
+  },
 ];
 
 const followMeItems = [
   {
-    label: 'Github',
-    href: 'https://github.com/technikhil314',
+    label: "Github",
+    href: "https://github.com/technikhil314",
   },
   {
-    label: 'Stack Overflow',
-    href: 'https://stackoverflow.com/users/2503826/nikhil-mehta',
+    label: "Stack Overflow",
+    href: "https://stackoverflow.com/users/2503826/nikhil-mehta",
   },
   {
-    label: 'Twitter',
-    href: 'https://twitter.com/technikhil314',
+    label: "Twitter",
+    href: "https://twitter.com/technikhil314",
   },
   {
-    label: 'Linkedin',
-    href: 'https://www.linkedin.com/in/technikhil314/',
+    label: "Linkedin",
+    href: "https://www.linkedin.com/in/technikhil314/",
   },
   {
-    label: 'Youtube',
-    href: 'https://www.youtube.com/channel/UCgwTt4Vw5Azo8yCr2HUvbYQ',
+    label: "Youtube",
+    href: "https://www.youtube.com/channel/UCgwTt4Vw5Azo8yCr2HUvbYQ",
   },
 ];
 
 module.exports = {
   title: "Nikhil Mehta",
   tagline: "Nikhil Mehta's Personal website / portfolio / blog.",
-  url: 'https://technikhil314.netlify.app',
-  baseUrl: '/',
+  url: "https://technikhil314.netlify.app",
+  baseUrl: "/",
   trailingSlash: false,
-  favicon: '/favicon.ico',
-  organizationName: 'technikhil314', // Usually your GitHub org/user name.
-  projectName: 'Portfolio for technikhil314', // Usually your repo name.
-  onBrokenLinks: 'warn',
-  onDuplicateRoutes: 'throw',
+  favicon: "/favicon.ico",
+  organizationName: "technikhil314", // Usually your GitHub org/user name.
+  projectName: "Portfolio for technikhil314", // Usually your repo name.
+  onBrokenLinks: "warn",
+  onDuplicateRoutes: "throw",
   themeConfig: {
     colorMode: {
-      defaultMode: 'dark'
+      defaultMode: "dark",
     },
     announcementBar: {
-      id: 'currently_working', // Any value that will identify this message.
+      id: "currently_working", // Any value that will identify this message.
       content:
         'Currently I am building a no server & privacy focused <a href="https://diffviewer.vercel.app/">text diff viewer</a>. Please check it out <a href="https://github.com/technikhil314/offline-diff-viewer/stargazers">star</a> & share it. You can find the motivation behind this tool <a href="https://github.com/technikhil314/offline-diff-viewer#motivation">here<a/>',
-      backgroundColor: '#fafbfc', // Defaults to `#fff`.
-      textColor: '#091E42', // Defaults to `#000`.
+      backgroundColor: "#fafbfc", // Defaults to `#fff`.
+      textColor: "#091E42", // Defaults to `#000`.
       isCloseable: true, // Defaults to `true`.
     },
     navbar: {
       hideOnScroll: true,
-      title: 'TechNikhil',
+      title: "TechNikhil",
       logo: {
-        alt: 'TechNikhil314',
-        src: '/logo.png',
+        alt: "TechNikhil314",
+        src: "/logo.png",
       },
       items: [
         {
-          label: 'My Work',
-          position: 'left',
-          items: myopenSourceWorkItems
+          label: "My Work",
+          position: "left",
+          items: myopenSourceWorkItems,
         },
-        { to: 'blog', label: 'Blog', position: 'left' },
+        { to: "blog", label: "Blog", position: "left" },
         {
-          to: 'about', label: 'About me', position: 'left'
+          to: "about",
+          label: "About me",
+          position: "left",
         },
-        { to: 'contactme', label: 'Contact me', position: 'left' },
-        { href: 'https://www.getrevue.co/profile/technikhil314', label: 'Newsletter', position: "right" },
+        { to: "contactme", label: "Contact me", position: "left" },
+        { to: "poems", label: "Poems", position: "right" },
+        {
+          href: "https://www.getrevue.co/profile/technikhil314",
+          label: "Newsletter",
+          position: "right",
+        },
         {
           label: "Sponsor me",
-          position: 'right',
-          items: sponsorMeItems
+          position: "right",
+          items: sponsorMeItems,
         },
         {
           label: "Follow me",
-          position: 'right',
-          items: followMeItems
+          position: "right",
+          items: followMeItems,
         },
-        { href: `${process.env.HOST_NAME}/Nikhil_Mehta.pdf`, label: 'My resume', position: 'right' },
+        {
+          href: `${process.env.HOST_NAME}/Nikhil_Mehta.pdf`,
+          label: "My resume",
+          position: "right",
+        },
       ],
     },
     footer: {
       logo: {
-        alt: 'technikhil314 logo',
-        src: '/brand-430x495.png',
-        href: '/',
+        alt: "technikhil314 logo",
+        src: "/brand-430x495.png",
+        href: "/",
       },
       links: [
         {
-          title: 'My Work',
-          items: myopenSourceWorkItems
+          title: "My Work",
+          items: myopenSourceWorkItems,
         },
         {
-          title: 'Navigation',
+          title: "Navigation",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog",
             },
             {
-              label: 'About me',
-              to: '/about',
+              label: "About me",
+              to: "/about",
             },
             {
-              label: 'Newsletter',
-              href: 'https://www.getrevue.co/profile/technikhil314',
+              label: "Newsletter",
+              href: "https://www.getrevue.co/profile/technikhil314",
             },
             {
-              label: 'Contact me',
-              href: '/mailto:nikhil.001mehta@gmail.com'
-            }
+              label: "Contact me",
+              href: "/mailto:nikhil.001mehta@gmail.com",
+            },
           ],
         },
         {
-          title: 'Follow me',
-          items: followMeItems
+          title: "Follow me",
+          items: followMeItems,
         },
         {
-          title: 'Sponsor me',
+          title: "Sponsor me",
           items: sponsorMeItems,
         },
       ],
-      copyright: `Copyright © 2020${new Date().getFullYear() === 2020 ? '' : `-${new Date().getFullYear()}`} Nikhil Mehta.`
+      copyright: `Copyright © 2020${
+        new Date().getFullYear() === 2020 ? "" : `-${new Date().getFullYear()}`
+      } Nikhil Mehta.`,
     },
   },
   plugins: [
-    'docusaurus2-dotenv',
+    "docusaurus2-dotenv",
+    [
+      "@docusaurus/plugin-content-blog",
+      {
+        id: "poems",
+        path: "poems",
+        routeBasePath: "poems",
+        editUrl: "https://github.com/technikhil314/www/edit/master/",
+        postsPerPage: 1,
+      },
+    ],
     async function docusaurus() {
       return {
         name: "docusaurus-tailwindcss",
@@ -169,84 +199,88 @@ module.exports = {
       };
     },
     [
-      '@docusaurus/plugin-pwa',
+      "@docusaurus/plugin-pwa",
       {
-        debug: process.env.DEBUG === 'true',
-        offlineModeActivationStrategies: ['appInstalled', 'queryString'],
+        debug: process.env.DEBUG === "true",
+        offlineModeActivationStrategies: ["appInstalled", "queryString"],
         pwaHead: [
           {
-            tagName: 'link',
-            rel: 'manifest',
-            href: '/manifest.json'
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json",
           },
           {
-            tagName: 'meta',
-            name: 'theme-color',
-            content: '#58C3A0'
+            tagName: "meta",
+            name: "theme-color",
+            content: "#58C3A0",
           },
           {
-            tagName: 'meta',
-            name: 'apple-mobile-web-app-capable',
-            content: 'yes'
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
           },
           {
-            tagName: 'meta',
-            name: 'apple-mobile-web-app-status-bar-style',
-            content: '#58C3A0'
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#58C3A0",
           },
           {
-            tagName: 'link',
-            rel: 'apple-touch-icon',
-            href: '/180x180.png'
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "/180x180.png",
           },
           {
-            tagName: 'link',
-            rel: 'mask-icon',
-            href: '/180x180.png'
-          }
+            tagName: "link",
+            rel: "mask-icon",
+            href: "/180x180.png",
+          },
         ],
       },
     ],
   ],
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         // debug: true,
         gtag: {
-          trackingID: 'GTM-5HBRZXW',
+          trackingID: "GTM-5HBRZXW",
           anonymizeIP: true,
         },
         googleAnalytics: {
-          trackingID: 'UA-125781207-2',
+          trackingID: "UA-125781207-2",
           anonymizeIP: true,
         },
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl: 'https://github.com/technikhil314/www/edit/master/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/technikhil314/www/edit/master/",
           sidebarCollapsible: true,
-          sidebarCollapsed: false
+          sidebarCollapsed: false,
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: 'all',
-            copyright: `Copyright © 2020${new Date().getFullYear() === 2020 ? '' : `-${new Date().getFullYear()}`} Nikhil Mehta.`
+            type: "all",
+            copyright: `Copyright © 2020${
+              new Date().getFullYear() === 2020
+                ? ""
+                : `-${new Date().getFullYear()}`
+            } Nikhil Mehta.`,
           },
-          editUrl: 'https://github.com/technikhil314/www/edit/master/',
+          editUrl: "https://github.com/technikhil314/www/edit/master/",
           blogPostComponent: require.resolve("./src/components/blogPost.jsx"),
         },
         theme: {
           customCss: [
-            require.resolve('./node_modules/gitalk/dist/gitalk.css'),
-            require.resolve('./src/css/custom.css'),
-          ]
+            require.resolve("./node_modules/gitalk/dist/gitalk.css"),
+            require.resolve("./src/css/custom.css"),
+          ],
         },
         sitemap: {
-          changefreq: 'weekly',
-          priority: 0.5
-        }
-      }
-    ]
-  ]
+          changefreq: "weekly",
+          priority: 0.5,
+        },
+      },
+    ],
+  ],
 };
