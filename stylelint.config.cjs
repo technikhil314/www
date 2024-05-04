6a578370e0ebf8c6d01c8c922c0b24e21d261704
Nikhil Mehta <nikhil.001mehta@gmail.com>
2024-05-04 15:13:56 +0530

    chore: added more rules for stylelint

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,3 +1,19 @@
 module.exports = {
   extends: "stylelint-config-standard",
+  rules: {
+    "selector-class-pattern": null,
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+    "at-rule-no-unknown": [
+      true,
+      {
+        ignoreAtRules: ["extends", "tailwind"],
+      },
+    ],
+  },
+  ignoreFiles: ["build/**/*.css"],
 };
